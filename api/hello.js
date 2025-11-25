edd820ac5289bc143ea9bff45af0644fe413548d
sharda <Sharda7011@gmail.com>
2025-11-25 14:56:18 +0530

    Convert to ES modules for Vercel compatibility

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,4 @@
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -15,4 +15,4 @@ module.exports = async (req, res) => {
       has_base_url: !!process.env.BASE_URL
     }
   });
-};
+}
