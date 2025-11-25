18536771619dd665d86590be0a0d4efeddd3882e
sharda <Sharda7011@gmail.com>
2025-11-25 14:33:39 +0530

    Add simple test API function

---
+++ api/hello.js
@@ -0,0 +1,18 @@
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  res.status(200).json({ 
+    message: 'Hello from Vercel API!', 
+    timestamp: new Date().toISOString(),
+    env_check: {
+      has_api_key: !!process.env.API_KEY,
+      has_base_url: !!process.env.BASE_URL
+    }
+  });
+};
